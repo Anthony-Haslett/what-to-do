24db27555aa62efe74761c640f792813994f7066
Tony Deverill <DevTony72@gmail.com>
2016-07-19 21:08:01 +0100

    changed facilities to be a table. Added bottom border to adjust results button.

--- js/index-mobile-tab-toggle.js
+++ js/index-mobile-tab-toggle.js
@@ -17,16 +17,20 @@ function clearHide()
 /* Toggle content based on tab links in header */
 $( "#custom-search-link" ).click(function() {  		/* on click of custom search link */
     if($('#custom-search:visible').length){}		/* if the custom search content is visible do nothing */
-    else { 											/* else, hide region content and show custom search content */
+    else {
+        $('#search-region-link').removeClass('nav-selected');  											/* else, hide region content and show custom search content */
      	$('#region-search').hide();
-        $('#custom-search').show();      
+        $('#custom-search').show();
+        $('#custom-search-link').addClass('nav-selected');       
     }
 });
 
 $( "#search-region-link" ).click(function() { 		/* on click of region search link */
     if($('#region-search:visible').length){}		/* if the region search content is visible do nothing */
-    else {											/* else, hide custom search content and show region content */
+    else {	
+        $('#custom-search-link').removeClass('nav-selected');										/* else, hide custom search content and show region content */
      	$('#custom-search').hide();
-        $('#region-search').show();    
+        $('#region-search').show();  
+        $('#search-region-link').addClass('nav-selected');   
     }    
 });
