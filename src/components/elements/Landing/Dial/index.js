cb14adeb6501ec7fe7dbeabe430725597a8e40b4
John B <johnbere@live.co.uk>
2017-02-18 16:05:17 +0000

    Adapted Neil's code from his old slider components

--- src/components/elements/Landing/Dial/index.js
+++ src/components/elements/Landing/Dial/index.js
@@ -1,12 +1,134 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Cursor from './Cursor';
+import $ from 'jquery';
 
 class Dial extends Component {
+  constructor(props) {
+      super(props);
+      
+      this.state = {
+          isMovable: false,
+          
+          cursorPosition: {
+              x: 0,
+              y: 0
+          },
+          
+          sliderPosition: {
+              x: parseInt($(this.refs.sliderCursor).css('left')),
+              y: parseInt($(this.refs.sliderCursor).css('top'))
+          }
+      }
+  }
+  
+  componentDidMount () {
+    $(this.refs.sliderContainer).mousemove(this.onCursorMove.bind(this));
+    $(this.refs.sliderCursor).mousedown(this.sliderDown.bind(this));
+    $(this.refs.sliderContainer).mouseup(this.sliderUp.bind(this));
+    
+    // $(this.refs.sliderContainer).bind('touchdown', this.onTouchMove.bind(this));
+    // $(this.refs.sliderContainer).bind('touchstart', this.sliderDown.bind(this));
+    // $(this.refs.sliderContainer).bind('touchend', this.sliderUp.bind(this));
+  }
+  
+  componentWillUnmount () {
+    $(this.refs.sliderContainer).unbind(this.onCursorMove.bind(this));
+    $(this.refs.sliderCursor).unbind(this.sliderDown.bind(this));
+    $(this.refs.sliderContainer).unbind(this.sliderUp.bind(this));
+    
+    // $(this.refs.sliderContainer).unbind('touchdown', this.onTouchMove.bind(this));
+    // $(this.refs.sliderContainer).unbind('touchstart', this.sliderDown.bind(this));
+    // $(this.refs.sliderContainer).unbind('touchend', this.sliderUp.bind(this));
+  }
+  
+  sliderDown = (event) => {
+    this.setState({isMovable: true});  
+  }
+  
+  sliderUp = (event) => {
+    this.setState({isMovable: false});  
+  }
+  
+  onCursorMove = (event) => {
+    var offsets = {
+      left: this.refs.sliderContainer.offsetLeft + (this.refs.sliderCursor.clientWidth / 2)  
+    }
+    
+    var positionUpdate = {
+      x: event.pageX - offsets.left,
+      y: event.pageY
+    }
+    
+    if (positionUpdate.x < this.refs.sliderContainer.offsetLeft) {
+      positionUpdate.x = this.refs.sliderContainer.offsetLeft  
+    }
+    
+    if (positionUpdate.x > this.refs.sliderContainer.offsetLeft + this.refs.hr.clientWidth) {
+      positionUpdate.x = this.refs.sliderContainer.offsetLeft + this.refs.hr.clientWidth;  
+    }
+    
+    var isMovable = this.state.isMovable;
+    
+    if (!isMovable) {
+      return;
+    }
+    
+    this.setState({cursorPosition: positionUpdate}, () => {
+      this.updateSliderPosition(positionUpdate);  
+    });
+  }
+  
+  // onTouchMove = (event) => {
+  //   var touchEvent = event.touches[0];
+    
+  //   var offsets = {
+  //     left: this.refs.sliderContainer.offsetLeft + (this.refs.sliderCursor.clientWidth / 2)  
+  //   }
+    
+  //   var positionUpdate = {
+  //     x: touchEvent.pageX - offsets.left,
+  //     y: touchEvent.pageY
+  //   }
+    
+  //   if (positionUpdate.x < this.refs.sliderContainer.offsetLeft) {
+  //     positionUpdate.x = this.refs.sliderContainer.offsetLeft  
+  //   }
+    
+  //   if (positionUpdate.x > this.refs.sliderContainer.offsetLeft + this.refs.hr.clientWidth) {
+  //     positionUpdate.x = this.refs.sliderContainer.offsetLeft + this.refs.hr.clientWidth;  
+  //   }
+    
+  //   var isMovable = this.state.isMovable;
+    
+  //   if (!isMovable) {
+  //     return;
+  //   }
+    
+  //   this.setState({cursorPosition: positionUpdate}, () => {
+  //     this.updateSliderPosition(positionUpdate);  
+  //   });
+  // }
+  
+  // onTouchStart = (event) => {
+  //   this.setState({isMovable: true});  
+  // }
+  
+  // onTouchEnd = (event) => {
+  //   this.setState({isMovable: false});  
+  // }
+  
+  updateSliderPosition = (position) => {
+    
+    console.debug(position);
+    $(this.refs.sliderCursor).css('left', position.x + 'px');  
+  }
+  
   render() {
     return (
-      <div className="dial-wrapper">
-        <Cursor />
+      <div className="dial-wrapper" refs="sliderContainer">
+        <hr class="header-wrapper" refs="sliderHeader" />
+        <Cursor refs="sliderCursor" />
       </div>
     )
   }
